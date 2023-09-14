82a66add4fcad3ceeddd05a1838f6dfb35602f32
Alfredo-Vqz <a01656384@tec.mx>
2023-09-14 17:32:46 -0600

    base de datos sirviendo

--- BACK/model/usermodel.py
+++ BACK/model/usermodel.py
@@ -6,7 +6,7 @@ from database import Base
 class User(Base):
     __tablename__ = "user"
 
-    id = Column(Integer, primary_key=True, index=True)
+    iduser = Column(Integer, primary_key=True, index=True)
     name = Column(String)
     last_name=Column(String)
     email = Column(String)
