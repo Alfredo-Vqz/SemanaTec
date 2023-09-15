9525a2040989f334bbfbf0db75b90448130cd25d
Alfredo-Vqz <a01656384@tec.mx>
2023-09-15 13:21:34 -0600

    bCK

--- BACK/repository/reminderrepository.py
+++ BACK/repository/reminderrepository.py
@@ -1,10 +1,10 @@
 from sqlalchemy.orm import Session
 
-from model import usermodel
+from model import remindermodel
 from schema import reminderschema
 
 def create_reminder(db: Session, reminder: reminderschema.Reminder):
-    db_reminder = usermodel.Reminder(email=reminder.email,title=reminder.title,description=reminder.description, datetime=reminder.datetime)
+    db_reminder = remindermodel.Reminder(email=reminder.email,title=reminder.title,description=reminder.description, datetime=reminder.datetime)
     db.add(db_reminder)
     db.commit()
     db.refresh(db_reminder)
