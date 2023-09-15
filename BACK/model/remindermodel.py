c10018638dc953256e1b3799c5a642d8882688e9
Alfredo-Vqz <a01656384@tec.mx>
2023-09-15 13:25:01 -0600

    CAMBIOS

--- BACK/model/remindermodel.py
+++ BACK/model/remindermodel.py
@@ -3,7 +3,7 @@ from sqlalchemy.sql import func
 from database import Base
 
 class Reminder(Base):
-    __tablemame__ = "reminder"
+    __tablename__ = "reminder"
 
     idreminder = Column(Integer, primary_key=True, index=True)
     email = Column(String)
